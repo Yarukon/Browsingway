8dbdc1f45fcff3fae4929e076e540e24bca3ec3f
Yarukon <a2692866561@live.com>
2025-06-25 13:28:29 +0800

    PersistUserPref

--- Browsingway.Renderer/CefHandler.cs
+++ Browsingway.Renderer/CefHandler.cs
@@ -12,13 +12,15 @@ internal static class CefHandler
 	{
 		CefSettings settings = new()
 		{
-			BrowserSubprocessPath = Path.Combine(cefAssemblyPath, "CefSharp.BrowserSubprocess.exe"), RootCachePath = cefCacheDir,
+			BrowserSubprocessPath = Path.Combine(cefAssemblyPath, "CefSharp.BrowserSubprocess.exe"),
+			RootCachePath = cefCacheDir,
 #if !DEBUG
 			LogSeverity = LogSeverity.Fatal,
 #endif
 		};
 		RootCachePath = settings.RootCachePath;
 		settings.CefCommandLineArgs["autoplay-policy"] = "no-user-gesture-required";
+		settings.CefCommandLineArgs["persist-user-preferences"] = "1";
 		settings.EnableAudio();
 		settings.SetOffScreenRenderingBestPerformanceArgs();
 		settings.UserAgentProduct = $"Chrome/{Cef.ChromiumVersion} Browsingway/{Assembly.GetEntryAssembly()?.GetName().Version} (ffxiv_pid {parentPid}; renderer_pid {Environment.ProcessId})";
